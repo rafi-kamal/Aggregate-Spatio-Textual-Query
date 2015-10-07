26b60fc7a5aa665c7c54c78f1651e2b2b80a1513
Rafi Kamal <rafikamal93@gmail.com>
2015-10-08 05:49:12 +0600

    yelp dataset parameters added

---
+++ src/utils/Parameters.java
@@ -0,0 +1,17 @@
+package utils;
+
+public interface Parameters {
+	
+	// Parameters for Yelp dataset
+	final double latitudeStart = 32.871;
+	final double latitudeEnd = 56.03655;
+	final double longitudeStart = -111.253141;
+	final double longitudeEnd = -89.59568;
+	
+	final double uniqueKeywords = 783;
+	final double maxWeight = 0.82478;
+	
+	final double maxD = Math.sqrt((latitudeEnd - latitudeStart) * (latitudeEnd - latitudeStart)
+			+ (longitudeEnd - longitudeStart) * (longitudeEnd - longitudeStart));
+	
+}
